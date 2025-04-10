91766e38f59ccac8cd3238680dfe27bb752a5c3d
IDeal <cyon256@icloud.com>
2025-04-11 07:52:00 +0900

    clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+use clap::{
+    Args,
+    Parser,
+    Subcommand,
+};
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+#[command(propagate_version = true)]
+struct Cli {
+    #[command(subcommand)]
+    command: Option<Commands>, 
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Adds files to myapp
+    Add(AddArgs),
+}
+
+#[derive(Args)]
+struct AddArgs {
+    name: Option<String>,
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Some(Commands::Add(name)) => {
+            println!("'myapp add' was used, name is : {:?}", name.name);
+        }
+        None => {
+            println!("__");
+        }
+    }
 }
