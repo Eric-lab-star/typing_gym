3df734d3d189f4f2556325b19816363f62946a44
IDeal <cyon256@icloud.com>
2025-04-11 08:28:04 +0900

    clap

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,76 @@
 use clap::{
-    Args,
     Parser,
-    Subcommand,
+    Args,
 };
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
-#[command(propagate_version = true)]
 struct Cli {
-    #[command(subcommand)]
-    command: Option<Commands>, 
-}
+    #[command(flatten)]
+    vers: Vers,
 
-#[derive(Subcommand)]
-enum Commands {
-    /// Adds files to myapp
-    Add(AddArgs),
+
+    /// Some regular input
+    #[arg(group = "input")]
+    input_file: Option<String>,
+
+    /// some special input argument
+    #[arg(long, group = "input")]
+    spec_in: Option<String>,
+
+    #[arg(short, requires = "input")]
+    config: Option<String>,
 }
 
 #[derive(Args)]
-struct AddArgs {
-    name: Option<String>,
-}
+#[group(required = true, multiple = false)]
+struct Vers {
+    #[arg(long, value_name = "VER")]
+    set_ver: Option<String>,
 
+    /// auto inc major
+    #[arg(long)]
+    major: bool,
+
+    /// auto inc minor
+    #[arg(long)]
+    minor: bool,
+
+    /// auto inc patchk
+    #[arg(long)]
+    patch: bool,
+}
 
 fn main() {
     let cli = Cli::parse();
 
-    match &cli.command {
-        Some(Commands::Add(name)) => {
-            println!("'myapp add' was used, name is : {:?}", name.name);
-        }
-        None => {
-            println!("__");
-        }
+    let mut major = 1;
+    let mut minor = 2;
+    let mut patch = 3;
+
+    let vers = &cli.vers;
+    let version = if let Some(ver) = vers.set_ver.as_deref() {
+        ver.to_string()
+    } else {
+        let (maj, min, pat) = (vers.major, vers.minor, vers.patch);
+        match (maj, min, pat) {
+            (true, _, _) => major += 1,
+            (_, true, _) => minor += 1,
+            (_, _, true) => patch += 1,
+            _ => unreachable!(),
+        };
+        format!("{major}.{minor}.{patch}")
+    };
+
+    print!("Version: {version}");
+
+    if let Some(config) = cli.config.as_deref() {
+        let input = cli
+            .input_file
+            .as_deref()
+            .unwrap_or_else(|| cli.spec_in.as_deref().unwrap());
+        println!("Doing work using input {input} and config {config}");
     }
+
+
 }
